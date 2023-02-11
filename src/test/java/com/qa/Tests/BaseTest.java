2a6cb29d2b5277f9716e72fada364f691488616c
Situ <situ.education@gmail.com>
2023-02-11 17:28:19 +0530

    Added two source folders

--- src/test/java/com/qa/Tests/BaseTest.java
+++ src/test/java/com/qa/Tests/BaseTest.java
@@ -1,24 +1,14 @@
 package com.qa.Tests;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.asserts.SoftAssert;
 
 import com.Utility.ReadData;
 import com.Utility.UtilityMethods;
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.Status;
-import com.aventstack.extentreports.markuputils.ExtentColor;
-import com.aventstack.extentreports.markuputils.MarkupHelper;
-import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.qa.BasePages.BasePage;
 import com.qa.BasePages.Page;
 import com.qa.Pages.LoginPage;
